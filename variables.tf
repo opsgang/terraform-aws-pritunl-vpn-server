db5a63dcff763b840911d81879aee201bf3cb6a5
Josh Russell <979683+j-russell@users.noreply.github.com>
2020-11-10 00:21:25 -0500

    Updates to support terraform 0.13

--- variables.tf
+++ variables.tf
@@ -16,18 +16,18 @@ variable "ami_id" {
 
 variable "instance_type" {
   description = "Instance type for VPN Box"
-  type        = "string"
+  type        = string
   default     = "t2.micro"
 }
 
 variable "whitelist" {
   description = "[List] Office IP CIDRs for SSH and HTTPS"
-  type        = "list"
+  type        = list(string)
 }
 
 variable "whitelist_http" {
   description = "[List] Whitelist for HTTP port"
-  type        = "list"
+  type        = list(string)
   default     = ["0.0.0.0/0"]
 }
 
@@ -48,7 +48,7 @@ variable "healthchecks_io_key" {
 
 variable "internal_cidrs" {
   description = "[List] IP CIDRs to whitelist in the pritunl's security group"
-  type        = "list"
+  type        = list(string)
   default     = ["10.0.0.0/8"]
 }
 
